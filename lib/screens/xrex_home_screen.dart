66d51bc3b7c52002be7f78680c51f5fde4178281
Furkan <blackredpodium@gmail.com>
2026-06-13 14:28:55 +0300

    Fix web image path handling

--- lib/screens/xrex_home_screen.dart
+++ lib/screens/xrex_home_screen.dart
@@ -1,6 +1,5 @@
-import 'dart:typed_data';
-
 import 'package:file_picker/file_picker.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import '../models/xrex_catalog_session.dart';
@@ -68,7 +67,7 @@ class _XRexHomeScreenState extends State<XRexHomeScreen> {
 
       setState(() {
         selectedImageBytes = bytes;
-        selectedImagePath = result?.files.single.path;
+        selectedImagePath = kIsWeb ? null : result?.files.single.path;
         isPicking = false;
       });
     } catch (error) {
